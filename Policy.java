adf6215d8e1bbbc3dd606c7ca06250a7aee557d5
Joseph-Vysotsky <jvysots0@email.cpcc.edu>
2023-04-05 11:34:18 -0400

    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -4,7 +4,7 @@ public class Policy {
    private String phFirstName;
    private String phLastName;
    private int phAge;
-   private String phSmokeStat; //"smoker" or "non-smoker"
+   private String phSmokingStatus; //"smoker" or "non-smoker"
    private double phHeight; //in Inches
    private double phWeight; //in Pounds
    
@@ -15,50 +15,68 @@ public class Policy {
       this.phFirstName     = "";
       this.phLastName      = "";
       this.phAge           = 0;
-      this.phSmokeStat = "non-smoker";
+      this.phSmokingStatus = "non-smoker";
       this.phHeight        = 0.00;
       this.phWeight        = 0.00;
    }
    
-   public Policy(int policyNumber, String providerName, String phFirst, String phLast, int phAge, String phSmokeStat, double phHeight, double phWeight) {
+   public Policy(int policyNumber, String providerName, String phFirst, String phLast, int phAge, String phSmokingStatus, double phHeight, double phWeight) {
       this.policyNumber    = policyNumber;
       this.providerName    = providerName;
       this.phFirstName     = phFirst;
       this.phLastName      = phLast;
       this.phAge           = phAge;
-      this.phSmokeStat = phSmokeStat;
+      this.phSmokingStatus = phSmokingStatus;
       this.phHeight        = phHeight;
       this.phWeight        = phWeight;
    }
    
    //Accessor methods
+   /** @return the policy number */
    public int getPolicyNumber()                 { return policyNumber; }
+   /** @return the provider's name */
    public String getProviderName()              { return providerName; }
+   /** @return the policyholder's first name */
    public String getPolicyholderFirstName()     { return phFirstName; }
+   /** @return the policyholder's last name */
    public String getPolicyholderLastName()      { return phLastName; }
+   /** @return the policyholder's age */
    public int getPolicyholderAge()              { return phAge; }
-   public String getPolicyholderSmokingStatus() { return phSmokeStat; }
+   /** @return the smoking status of the policyholder */
+   public String getPolicyholderSmokingStatus() { return phSmokingStatus; }
+   /** @return the policyholder's height in inches */
    public double getPolicyholderHeight()        { return phHeight; }
+   /** @return the policyholder's weight in pounds */
    public double getPolicyholderWeight()        { return phWeight; }
    
    //Mutator methods
+   /** @param pn the policy number */
    public void setPolicyNumber(int pn)                   { this.policyNumber = pn; }
+   /** @param pn the provider's name */
    public void setProviderName(String pn)                { this.providerName = pn; }
+   /** @param fn the policyholder's first name */
    public void setPolicyholderFirstName(String fn)       { this.phFirstName = fn; }
+   /** @param ln the policyholder's last name */
    public void setPolicyholderLastName(String ln)        { this.phLastName = ln; }
+   /** @param age the policyholder's age */
    public void setPolicyholderAge(int age)               { this.phAge = age; }
-   public void setPolicyholderSmokingStatus(String ss)   { this.phSmokeStat = ss; }
+   /** @param ss the policyholder's smoking status either "smoker" or "non-smoker" */
+   public void setPolicyholderSmokingStatus(String ss)   { this.phSmokingStatus = ss; }
+   /** @param h the policyholder's height in inches */
    public void setPolicyholderHeight(double h)           { this.phHeight = h; }
+   /** @param w the policyholder's weight in pounds */
    public void setPolicyholderWeight(double w)           { this.phWeight = w; }
    
-   //Calculates the BMI
+   //Calculating methods
+   /** @return the BMI of the policyholder */
    public double calcBMI() {
       return (this.phWeight * 703) / (this.phHeight * this.phHeight);
    }
+   /** @return the price of the policy */
    public double calcPolicyPrice() {
       double price = 600;
       if (this.phAge > 50) price += 75;
-      if (this.phSmokeStat == "smoker") price += 100;
+      if (this.phSmokingStatus == "smoker") price += 100;
       double bmi = this.calcBMI();
       if (bmi > 35) price += (bmi - 35) * 20;
       return price;
